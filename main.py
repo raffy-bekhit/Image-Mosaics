939320c77286c88cf6af107838cf1b8f2f293d8b
raffy-bekhit <raffybekhit@gmail.com>
2018-12-23 23:56:39 +0200

    final commit

--- main.py
+++ main.py
@@ -9,6 +9,12 @@ import random
 from PIL import Image;
 
 
+CORRESPONDENCE_AUT0 = 0 
+CORRESPONDENCE_MANUAL = 1
+CALCULATE_H_NORMALLY = 0
+CALCULATE_H_RANSAC = 1
+
+
 def calculate_h(p,p_):
     "takes setof points p and their corresponting points p_ , calculates the Homography matrix, returns 3x3 matrix"
     #p is array of correspondance points of image 1, p_ is same for image 2 
@@ -17,16 +23,7 @@ def calculate_h(p,p_):
     for i in range(p.shape[0]): #constructs A from given points of image 1 
         A[i*2] = p[i,0],p[i,1],1,0,0,0,-p[i,0]*p_[i,0],-p[i,1]*p_[i,0]    
         A[i*2+1] = 0,0,0, p[i,0],p[i,1],1,-p[i,0]*p_[i,1],-p[i,1]*p_[i,1] 
-#            for j in range(3): 
-#                if((i*3+j)%3 != 2 ):
-#                    A[i*3+j][j*3:(j*3)+2] = p[i][0],p[i][1]
-#                else:
-#                    A[i*3+j][j*3] = p[i][0]
-#                    A[i*3+j][j*3+1] = p[i][1]
-#         B=np.pad(B,(0,1),'constant',constant_values=1) #pad with ones to make it homogoneous
-#         B=B[0:-1,:]
-         
-         
+
     
     B=B.flatten().reshape(-1,1) # flatten and reshape to be one column for dimension suitability
     
@@ -34,7 +31,7 @@ def calculate_h(p,p_):
     
     H=np.append(H,1)  #puts 1 at the end of H
    
-    H = np.reshape(H,[3,3])
+    H = np.reshape(H,[3,3]) #return H as matrix of shape 3x3
     return H
 
 
@@ -53,7 +50,7 @@ def get_correspondance_auto(image1_gray,image2_gray):
     p=[] #list of correspondance point in first image
     p_ = [] #list of correspondance point in second image
     
-    for match in matches:
+    for match in matches: #loop on matches and fills p and p_
         index1 = match.queryIdx
         p.append((int(kp1[index1].pt[0]),int(kp1[index1].pt[1])) )
         
@@ -63,15 +60,14 @@ def get_correspondance_auto(image1_gray,image2_gray):
         
         
     
-    matchImg = cv.drawMatches(image1_gray,kp1,image2_gray,kp2,matches[0:20],image2_gray) #draws the matches on the image
-    cv.imwrite('Matches.png', matchImg)
+    #matchImg = cv.drawMatches(image1_gray,kp1,image2_gray,kp2,matches[0:20],image2_gray) #draws the best 20 matches on the image and saves it as output image
+    #cv.imwrite('Matches.png', matchImg)
     
     p=np.array(p)
     p_=np.array(p_)
     return p, p_
 
  
-
 
 
 def get_correspondance_manually(image1,image2,number_of_points):
@@ -88,26 +84,31 @@ def get_correspondance_manually(image1,image2,number_of_points):
         p1 = np.zeros([(number_of_points//2),2])
         p2 = np.zeros([number_of_points//2,2])
         pts = plt.ginput(n=number_of_points, timeout=0)
-        for i in range(0,number_of_points-1):
-             p1[i//2] = pts[i]
-        for i in range(1,number_of_points):
-            p2[i//2] = pts[i]
-		
-		
-        return p1,p2
-    
-    
+        p1_itr = 0
+        p2_itr = 0
+        for i in range(0, number_of_points):
+            if (i % 2 == 0):
+                p1[p1_itr] = pts[i]
+                print("p1 of index ", p1_itr, " is ", pts[i])
+                p1_itr += 1
+
+            else:
+                p2[p2_itr] = pts[i]
+                print("p2 of index ", p2_itr, " is ", pts[i])
+                p2_itr += 1
+        return p1, p2
+
 
 def InvTransform(h,point):
+    "for a point in second image gets its correspondance in first image"
 
-
-    inverse_point = np.dot(np.linalg.inv(h), point);
-    inverse_point[0] /= inverse_point[2]
+    inverse_point = np.dot(np.linalg.inv(h), point); #matrix multiplication with the inverse of H
+    inverse_point[0] /= inverse_point[2] #divides by weight
     inverse_point[1] /= inverse_point[2]
     return inverse_point[0:2]
 
 def transform(h,point):
-    
+    "for a point in first image gets its correspondance in second image "
     corr_point = np.dot(h, point);
     corr_point[0] /= corr_point[2];
     corr_point[1] /= corr_point[2]
@@ -122,25 +123,29 @@ def from_homo(points):
     return points[:,0:2]
     
 def warp(source_image,dest_image,h):
+    "aligns the two images"
     source_height = source_image.shape[0]
     source_width = source_image.shape[1] 
-    source_edges = np.array([[0,0],[source_width-1, 0],[0, source_height -1],[source_width-1, source_height-1]]) #get position of corners  of source image
-    source_edges= np.pad(source_edges,(0,1),'constant',constant_values=1)     #pad 1 to make it homogoneous
+    source_edges = np.array([[0,0],[source_width-1, 0],[0, source_height -1],[source_width-1, source_height-1]]) # array containing corner points of source image
+    source_edges= np.pad(source_edges,(0,1),'constant',constant_values=1)     #pad 1 to make it homogoneous representation
     source_edges = source_edges[:-1] #remove a redudant row from padding
 
     corr_source_edges = from_homo(np.dot(h,source_edges.T).T) #get the points cerresponding to edges in non-homogoneous form each row is a point and each colum is x or y 
-
-    max_mapped_i,max_mapped_j = int(np.ndarray.max(corr_source_edges[:,1],axis=0)),int(np.ndarray.max(corr_source_edges[:,0],axis=0))
+    
+    #get the corresponting positions of the corner points
+    max_mapped_i,max_mapped_j = int(np.ndarray.max(corr_source_edges[:,1],axis=0)),int(np.ndarray.max(corr_source_edges[:,0],axis=0)) 
     min_mapped_i,min_mapped_j =int( np.ndarray.min(corr_source_edges[:,1],axis=0)),int(np.ndarray.min(corr_source_edges[:,0],axis=0))
         
     
-
+    #gets the size of source image after warping
     mapped_source_height = max_mapped_i-min_mapped_i+1  
     mapped_source_width = max_mapped_j-min_mapped_j+1
     
+    #determines the shift that happens to the second image
     shiftHeight = -min_mapped_i
     shiftWidth = - min_mapped_j
-
+    
+    # new image containing the first image after warping
     mapped_source_image = np.zeros((mapped_source_height,mapped_source_width,3), dtype=np.uint8);
 
     
@@ -152,11 +157,11 @@ def warp(source_image,dest_image,h):
             mapped_i = int(mapped_position[1])
            
             mapped_source_image[mapped_i+shiftHeight][mapped_j+shiftWidth] = source_image[i][j];
+            
+            
     #save forward warped image
     #the resulting image contains holes
-
-    
-    cv.imwrite("with holes.png",mapped_source_image)
+    ###cv.imwrite("with holes.png",mapped_source_image)
     #inverse warping to remove holes
     for i in range(0, mapped_source_height):
         for j in range(0, mapped_source_width):
@@ -172,40 +177,37 @@ def warp(source_image,dest_image,h):
                     low_j = int(inverse_mapped_j);
                     idistance = inverse_mapped_i - low_i;
                     jdistace = inverse_mapped_j - low_j;
+                    
+                    #interpolation for each channel
                     mapped_source_image[i][j][0] = (1-idistance)*(1-jdistace)*source_image[low_i][low_j][0] + (1-idistance)*(jdistace)*source_image[low_i][low_j+1][0] + (idistance)*(1-jdistace)*source_image[low_i+1][low_j][0] + (idistance)*(jdistace)*source_image[low_i+1][low_j+1][0]
                     mapped_source_image[i][j][1] = (1-idistance)*(1-jdistace)*source_image[low_i][low_j][1] + (1-idistance)*(jdistace)*source_image[low_i][low_j+1][1] + (idistance)*(1-jdistace)*source_image[low_i+1][low_j][1] + (idistance)*(jdistace)*source_image[low_i+1][low_j+1][1]
                     mapped_source_image[i][j][2] = (1-idistance)*(1-jdistace)*source_image[low_i][low_j][2] + (1-idistance)*(jdistace)*source_image[low_i][low_j+1][2] + (idistance)*(1-jdistace)*source_image[low_i+1][low_j][2] + (idistance)*(jdistace)*source_image[low_i+1][low_j+1][2]
             
-
-    cv.imwrite("without holes.png",mapped_source_image)
-   	
-    
-    #merge the two images
+    #writes image after inverse warping
+    ###cv.imwrite("without holes.png",mapped_source_image)
+   	    
     dest_image_height = dest_image.shape[0]
     dest_image_width = dest_image.shape[1]
-
-
-
 	
-    
+    #get dimensions for the new image
     mosaic_image_height = max(mapped_source_height, dest_image_height+shiftHeight)
     mosaic_image_width= max(mapped_source_width, dest_image_width+shiftWidth)
 
     mosaic_image = np.zeros((mosaic_image_height, mosaic_image_width, 3), dtype=np.uint8);
-
+    
+    #put the second image in the mosaic image
     for i in range(0,dest_image_height):
         for j in range(0, dest_image_width):
-            #if not( i+ shiftHeight < mapped_source_height and j + shiftWidth < mapped_source_width):
             mosaic_image[i + shiftHeight][j + shiftWidth] = dest_image[i][j]
 
-	# sketch the destination image
+	#puts the first image in mosaic image
     for i in range(0,mapped_source_image.shape[0]):
         for j in range(0,mapped_source_image.shape[1]):
             if not( int(mapped_source_image[i][j][0]) == 0 and int(mapped_source_image[i][j][1]) == 0 and int(mapped_source_image[i][j][2]) == 0 ):
                 mosaic_image[i][j] = mapped_source_image[i][j]
+  
     
-    cv.imwrite("final_result.png",mosaic_image)
-    return
+    return mosaic_image
     
 
 def ransac_error(single_p,single_p_,h):
@@ -229,10 +231,7 @@ def ransac(p,p_,threshold,iterations):
    
     for i in range(iterations):
         inliners = 0 
-        
-        
-        
-        randp  = np.zeros([4,2])
+        randp  = np.zeros([4,2]) 
         randp_ = np.zeros([4,2])
         H = None
         for j in range(4):
@@ -249,58 +248,70 @@ def ransac(p,p_,threshold,iterations):
             error = ransac_error(p[j],p_[j],H)
            
             
-            if(error<threshold):
+            if(error<threshold): 
                 inliners+=1
             
             
         if(inliners>max_inliners):
             max_inliners = inliners
             best_h = H
-            
+            #return the H and the number of inliners for H
     return best_h , max_inliners 
             
     
+   
+def construct_mosaic(path1,path2,correspondance_method,H_calculation_method,ransac_loops=100):
+    image1 = cv.imread(path1)
+    image2 = cv.imread(path2)
+    
+    image1_gray = cv.cvtColor(image1,cv.COLOR_RGB2GRAY)
+    image2_gray = cv.cvtColor(image2,cv.COLOR_RGB2GRAY)
+    
+    new_filename = path1.split(".")[0]+"and"+path2.split(".")[0]
+    
+    corr_method_string = ""
+    H_method_string = ""
+    if(correspondance_method == CORRESPONDENCE_AUT0):
+        p1,p2 = get_correspondance_auto(image1_gray,image2_gray)
+        corr_method_string = "automatic correspondence"
+    else:
+        p1,p2 = get_correspondance_manually(image1,image2,8)
+        corr_method_string = "manual correspondence"
+
+    new_filename = new_filename +"_"+corr_method_string
+       
+    if(H_calculation_method==CALCULATE_H_NORMALLY):
+        
+        H = calculate_h(p1[0:90],p2[0:90])
+        H_method_string = "H calcualated without ransac"
+        
+        
+    else:
+        H, inliners = ransac(p1,p2,5,ransac_loops)
+        H_method_string = "H calcualated with ransac"
+        new_filename = new_filename +"_"+H_method_string + "_" + "ransacloops="+str(ransac_loops)
+    
+    new_filename = new_filename +"_"+H_method_string 
     
 
-image1 = cv.imread("./image1.png")
-image2 = cv.imread("./image2.png")
 
-image1_gray = cv.cvtColor(image1,cv.COLOR_RGB2GRAY)
-image2_gray = cv.cvtColor(image2,cv.COLOR_RGB2GRAY)
-p,p_ = get_correspondance_auto(image1_gray,image2_gray)
+    mosaic_image = warp(image1,image2,H)
+    
+    #writes the output
+    cv.imwrite(new_filename+".png",mosaic_image)
+    print("writing "+new_filename+" image is done")
 
-#p1,p2 = get_correspondance_manually(image1,image2,8)
+ 
 
-H = calculate_h(p,p_)
-print(H)
-H2,asd = cv.findHomography(p[0:50], p_[0:50], cv.RANSAC,5.0)
-print("H@",H2)
-ransac_h, inliners = ransac(p,p_,5,5000*5)
-print("rash: ",ransac_h)
-
-warp(image1,image2,ransac_h)
-
-#inverse_warp(ransac_h,image1,image2)
-#inverse_warp(H,image1,image2)
-
-#pro = np.matmul(H,np.array([531,508,1]).reshape(-1,1))
-
-# Sort them in the order of their distance.
-#fig = plt.figure(figsize=(1,2))
-
-#fig.add_subplot(1,1,1);
-#plt.figure(1)
-#plt.imshow(image1,cmap='gray');
-#
-#n=plt.ginput(2)
-#
-#plt.show()
-#
-#print(n)
-#cv.waitKey(0); #press key to close image
-#cv.destroyAllWindows(); #destroys all windows
-##
-##
-##
-
-
+#construct_mosaic("1.png","2.png",CORRESPONDENCE_AUT0,CALCULATE_H_RANSAC,100)
+construct_mosaic("1.png","2.png",CORRESPONDENCE_MANUAL,CALCULATE_H_RANSAC,10)
+#construct_mosaic("1.png","2.png",CORRESPONDENCE_AUT0,CALCULATE_H_RANSAC,1000)   
+#construct_mosaic("1.png","2.png",CORRESPONDENCE_AUT0,CALCULATE_H_RANSAC,5000)
+####
+####
+#construct_mosaic("1.png","2.png",CORRESPONDENCE_AUT0,CALCULATE_H_RANSAC,100)
+#construct_mosaic("1.png","2.png",CORRESPONDENCE_AUT0,CALCULATE_H_NORMALLY)
+###
+###
+#construct_mosaic("3.jpg","4.jpg",CORRESPONDENCE_AUT0,CALCULATE_H_RANSAC,1000)
+#construct_mosaic("1.png","2.png",CORRESPONDENCE_AUT0,CALCULATE_H_NORMALLY)
